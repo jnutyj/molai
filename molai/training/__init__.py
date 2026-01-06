f1aa7fd436ad1e8b5b78daa144a34aee9be09a41
ytao <yujun.tao@outlook.com>
2026-01-06 11:12:24 -0500

    update init

--- molai/training/__init__.py
+++ molai/training/__init__.py
@@ -1,12 +1,11 @@
-# molai/training/__init__.py
-
-from .supervised import train_qsar_model
-from .generation import train_generator
-from .rl import rl_finetune_generator
+from .supervised import train_lstm_predictor
+from .generation import train_lstm_generator, train_vae 
+from .rl import policy_gradient_step 
 
 __all__ = [
-    "train_qsar_model",
-    "train_generator",
-    "rl_finetune_generator",
+    "train_lstm_predictor",
+    "train_lstm_generator",
+    "train_vae",
+    "policy_gradient_step",
 ]
 
