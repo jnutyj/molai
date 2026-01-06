291d7bdc1eecbbf548c1e7b3642c0cfce8ff0a1d
ytao <yujun.tao@outlook.com>
2026-01-06 12:29:11 -0500

    update score

--- molai/application/__init__.py
+++ molai/application/__init__.py
@@ -1,11 +1,13 @@
 from .generate import sample_smiles_lstm
-from .score import score_smiles
+from .score import score_smiles_batch, compute_qed, compute_sa
 from .filter import is_valid_smiles, filter_smiles
 from .constraints import apply_constraints
 
 __all__ = [
     "sample_smiles_lstm",
-    "score_smiles",
+    "score_smiles_batch",
+    "compute_qed",
+    "compute_sa",
     "is_valid_smiles",
     "filter_smiles",
     "apply_constraints",
--- molai/application/score.py
+++ molai/application/score.py
@@ -1,16 +1,16 @@
-# molai/application/score.py
-
 import torch
 from typing import List
 from molai.data.smiles import SmilesTokenizer
-
+from rdkit import Chem
+from rdkit.Chem import QED
+import sascorer
 
 ########################################
 # PROPERTY SCORING
 ########################################
 
 @torch.no_grad()
-def score_smiles(
+def score_smiles_batch(
     smiles: List[str],
     model,
     tokenizer: SmilesTokenizer,
@@ -31,4 +31,44 @@ def score_smiles(
         scores.append(float(pred.item()))
 
     return scores
+
+
+
+
+
+@torch.no_grad()
+def score_smiles(
+    smi: str,
+    model,
+    tokenizer: SmilesTokenizer,
+    device: str = "cpu",
+):
+    """
+    Score SMILES using a trained predictor
+    """
+    model.eval()
+    model.to(device)
+
+
+
+    
+    ids = tokenizer.encode(smi)
+    x = torch.tensor(ids, dtype=torch.long).unsqueeze(0).to(device)
+    pred = model(x) ### TODO: need to know which model or based on model type for scoring. 
+    
+
+    return pred
+
+
+
+
+def compute_qed(smi:str):
+    mol=Chem.MolFromSmiles(smi)
+    return QED.qed(mol)
+
+
+def compute_sa(smi:str):
+    mol=Chem.MolFromSmiles(smi)
+    return sascorer.calculateScore(mol)
+
 
